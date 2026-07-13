655c3071462f33e3f97aff8a97d7d969c73621fa
Monster_777 <monsterearningtricks777@gmail.com>
2026-07-13 15:19:36 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+import Brevo from "@getbrevo/brevo";
+
+dotenv.config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+const apiInstance = new Brevo.TransactionalEmailsApi();
+
+apiInstance.setApiKey(
+    Brevo.TransactionalEmailsApiApiKeys.apiKey,
+    process.env.BREVO_API_KEY
+);
+
 app.get("/", (req, res) => {
-    res.send("MG E-Sport OTP Server Running");
-});
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-    console.log("Server Running on Port " + PORT);
+    res.send("MG E-Sport OTP Server Running ✅");
 });
